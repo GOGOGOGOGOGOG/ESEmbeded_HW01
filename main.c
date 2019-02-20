d89778760ad1d990f2b1a671801ab3bd115d62f9
GOGOGOGOGOGOG <gogo0929052207@gmail.com>
2019-02-20 18:41:26 +0800

    Second commit

--- main.c
+++ main.c
@@ -1,45 +1,18 @@
 #include <stdio.h>
-
-
 int minus(int a,int c)
-{
-   printf("the value is %3d",a-c);
-}
-
-
+{printf("the value is %3d\n",a-c);}
 int plus(int a,int c)
-{
-     printf("the value is %3d",a+c);
-}
+{printf("the value is %3d\n",a+c);}
 int multiply(int a, int c) 
-{ 
-   
-    printf("the value is %3d",a*c); 
-}
+{ printf("the value is %3d\n",a*c);}
 int divided(int a, int c) 
-{
-	
-    
-     printf("the value is %3f",(float)a/c); 
-}
-
-
-
-/* Our principal function. Takes two parameters, returns a function. */
+{ printf("the value is %3f\n",(float)a/c); }
 int (* show_type_attributes(char color) )(int,int)
-{
-   
-
-  
-    if (color == '+')
-        return plus;
-   if (color == '-')
-        return minus;
-   if (color == '*')
-        return multiply;       
-   if (color == '/')
-        return divided;
-
+{   
+    if (color == '+')return plus;
+   else if (color == '-') return minus;
+   else if (color == '*') return multiply;       
+   else if (color == '/') return divided;
 }
 //main() function
 int main()
@@ -47,11 +20,9 @@ int main()
     int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-   
-    int (*f)(int,int);  // Declare a pointer to a function with one parameter (int)
-
-    f = show_type_attributes(b); // f will take the return          
-    f(a,c);  
+	scanf("%i %c %i", &a, &b, &c); 
+    int (*f)(int,int);  // Declare a pointer to a function with two paramete
+    f = show_type_attributes(b); // f will take the return     
+    f(a,c);  // Call function that was returned by show_car_attributes()
     return 0;
 }
